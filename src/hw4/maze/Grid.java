5c8f841ed5bc2e4b7595009a5b694fd94dfce9ec
kenziemcallister <mackenziemcallister64@gmail.com>
2025-04-19 19:55:23 -0500

    Grid class
    - Grid class implemented with constructor, getters, setters, and tostring
    - passes all test cases in GridTests

---
+++ src/hw4/maze/Grid.java
@@ -0,0 +1,27 @@
+package hw4.maze;
+
+import java.util.List;
+
+public class Grid {
+	
+	private List<Row> rows; //list to hold each row within a grid
+
+	public Grid(List<Row> rows) {
+		super();
+		this.rows = rows;
+	}
+
+	public List<Row> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<Row> rows) {
+		this.rows = rows;
+	}
+
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
+
+}
