d055374508b9d0cc7502ad88cbe80193cd997baf
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-04-20 19:23:00 -0500

    Finish the Player Class
    Finished all of the methods for the Player class. Read the Javadoc for
    more information.

--- src/hw4/player/test/Player.java
+++ src/hw4/player/test/Player.java
@@ -8,6 +8,41 @@ public class Player {
 	Cell currentCell;
 	Row currentRow;
 	
+	/**
+	 * This is a parameterized constructor for the Player class
+	 * @param currentRow (Row)
+	 *     This argument represents the current row of the player.
+	 * @param currentCell (Cell)
+	 *     This argument represents the current cell of the player.
+	 * */
+	public Player(Row currentRow, Cell currentCell) {
+		this.currentCell = currentCell;
+		this.currentRow = currentRow;
+	}
+
+	/** Gets the current cell of the player. */
+	public Cell getCurrentCell() {
+		return currentCell;
+	}
+
+	/** Sets the current cell of the player. */
+	public void setCurrentCell(Cell currentCell) {
+		this.currentCell = currentCell;
+	}
+
+	/** Gets the current row of the player. */
+	public Row getCurrentRow() {
+		return currentRow;
+	}
+
+	/** Sets the current row of the player. */
+	public void setCurrentRow(Row currentRow) {
+		this.currentRow = currentRow;
+	}
+
+	/**
+	 * Displays information about the player.
+	 * */
 	@Override
 	public String toString() {
 		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
