5804fd5b919cc128e5b0493ed63e7238426b9273
kenziemcallister <mackenziemcallister64@gmail.com>
2025-04-23 01:15:13 -0500

    Grid JavaDocs
    - added javadocs for grid class

--- src/hw4/maze/test/Grid.java
+++ src/hw4/maze/test/Grid.java
@@ -6,19 +6,35 @@ public class Grid {
 	
 	private ArrayList<Row> rows; //list to hold each row within a grid
 
+	/**
+	 * Parameterized constructor for grid class.
+	 * @param rows - the list of rows being added to the grid.
+	 * rows contain cells.
+	 */
 	public Grid(ArrayList<Row> rows) {
 		super();
 		this.rows = rows;
 	}
 
+	/**
+	 * Getter for the row list within the grid.
+	 * @return
+	 */
 	public ArrayList<Row> getRows() {
 		return rows;
 	}
 
+	/**
+	 * Setter for the row list within the grid.
+	 * @param rows
+	 */
 	public void setRows(ArrayList<Row> rows) {
 		this.rows = rows;
 	}
 
+	/**
+	 * ToString method for Grid class.
+	 */
 	@Override
 	public String toString() {
 		return "Grid [rows=" + rows + "]";
