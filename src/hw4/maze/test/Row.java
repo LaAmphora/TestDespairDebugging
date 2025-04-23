ce12fbfe2d59d38776795c1aa3f1bb577dca4c33
kenziemcallister <mackenziemcallister64@gmail.com>
2025-04-23 01:16:28 -0500

    Row class JavaDocs
    - added row javadocs for each method

--- src/hw4/maze/test/Row.java
+++ src/hw4/maze/test/Row.java
@@ -7,18 +7,33 @@ public class Row {
 	//list to hold the cells in each row
 	private ArrayList<Cell> cells;
 
+	/**
+	 * Parameterized constructor for Row class.
+	 * @param cells - the list of cells being added to the row.
+	 */
 	public Row(ArrayList<Cell> cells) {
 		this.cells = cells;
 	}
 
+	/**
+	 * Getter for the list of cells for a row.
+	 * @return
+	 */
 	public ArrayList<Cell> getCells() {
 		return cells;
 	}
 
+	/**
+	 * Setter for the list of cells for a row.
+	 * @param cells
+	 */
 	public void setCells(ArrayList<Cell> cells) {
 		this.cells = cells;
 	}
 
+	/**
+	 * ToString for Row class.
+	 */
 	@Override
 	public String toString() {
 		return "Row [cells=" + cells + "]";
