599c53381365bb45ef8bc075bf68de593d338c6a
kenziemcallister <mackenziemcallister64@gmail.com>
2025-04-23 01:13:49 -0500

    Cell JavaDocs
    - added java docs for all cell methods

--- src/hw4/maze/test/Cell.java
+++ src/hw4/maze/test/Cell.java
@@ -7,6 +7,13 @@ public class Cell {
 	private CellComponents right;
 	private CellComponents left;
 	
+	/**
+	 * Parameterized Constructor for Cell class.
+	 * @param left - the left component of the cell being made
+	 * @param right - the right component of the cell being made
+	 * @param up - the up component of the cell being made
+	 * @param down - the down component of the cell being made
+	 */
 	public Cell(CellComponents left, CellComponents right, CellComponents up, CellComponents down) {
 		
 		if (up != null) { //if the provided component is not null, then set it as is.
@@ -35,10 +42,18 @@ public class Cell {
 		
 	}
 
+	/**
+	 * Getter for up component.
+	 * @return
+	 */
 	public CellComponents getUp() {
 		return up;
 	}
 
+	/**
+	 * Setter for up component.
+	 * @param up
+	 */
 	public void setUp(CellComponents up) {
 		if (up != null) { //need to set to edge (WALL) if we are setting the up component to be null
 			this.up = up;
@@ -47,10 +62,18 @@ public class Cell {
 		}
 	}
 
+	/**
+	 * Getter for down component.
+	 * @return
+	 */
 	public CellComponents getDown() {
 		return down;
 	}
 
+	/**
+	 * Setter for down component.
+	 * @param down
+	 */
 	public void setDown(CellComponents down) {
 		if (down != null) { //need to set to edge (WALL) if we are setting the down component to be null
 			this.down = down;
@@ -59,10 +82,18 @@ public class Cell {
 		}
 	}
 
+	/**
+	 * Getter for right component.
+	 * @return
+	 */
 	public CellComponents getRight() {
 		return right;
 	}
 
+	/**
+	 * Setter for right component.
+	 * @param right
+	 */
 	public void setRight(CellComponents right) {
 		if (right != null) { //need to set to edge (WALL) if we are setting the right component to be null
 			this.right = right;
@@ -71,10 +102,18 @@ public class Cell {
 		}
 	}
 
+	/**
+	 * Getter for left component.
+	 * @return
+	 */
 	public CellComponents getLeft() {
 		return left;
 	}
 
+	/**
+	 * Setter for left component.
+	 * @param left
+	 */
 	public void setLeft(CellComponents left) {
 		if (left != null) { //need to set to edge (WALL) if we are setting the left component to be null
 			this.left = left;
@@ -83,6 +122,9 @@ public class Cell {
 		}
 	}
 
+	/**
+	 * ToString method for Cell.
+	 */
 	@Override
 	public String toString() {
 	    return "Cell [left=" + left + ", right=" + right + ", up=" + up + ", down=" + down + "]";
